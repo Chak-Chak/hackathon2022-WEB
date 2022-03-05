77d5c8db7337cc00295b7b4b4460b188181b91d1
Егор Куцуев <ekutsuev@yandex.ru>
2022-03-05 16:59:55 +0500

    Updated auth page

--- src/Pages/AuthPage.js
+++ src/Pages/AuthPage.js
@@ -4,13 +4,17 @@ import {connect} from "react-redux";
 
 const AuthPageLayout = ({}) => {
     return (
-        <div className="d-flex justify-content-center align-items-center w-100" style={{height: window.innerHeight}}>
+        <div className="d-flex justify-content-center align-items-center w-100" style={{height: window.innerHeight-100}}>
             <div className="card w-25 p-3">
                 <div className="card-body">
                     <h5 className="card-title">Авторизация</h5>
                     <div className="input-group mt-4">
-                        <span className="input-group-text w-25">Логин</span>
-                        <input type="text" aria-label="First name" className="form-control"/>
+                        <select className="form-select" aria-label="Default select example">
+                            <option selected>Выберите пользователя</option>
+                            <option value="0">Куратор контракта</option>
+                            <option value="1">Отдел организации закупок</option>
+                            <option value="2">Планово-экономический отдел</option>
+                        </select>
                     </div>
                     <div className="input-group mt-3 mb-3">
                         <span className="input-group-text w-25">Пароль</span>
