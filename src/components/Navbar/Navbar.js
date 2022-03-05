b7aa6a8a803f90c1c62f98964577690385dacb12
NickPYTH <nicksrnk@gmail.com>
2022-03-05 16:16:56 +0500

    Added rr6

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,37 @@
 import React, {useEffect} from "react";
 import { bindActionCreators } from "redux";
 import {connect} from "react-redux";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect,
+    useRouteMatch,
+} from "react-router-dom";
 
 const NavbarLayout = ({info}) => {
     return (
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar</a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                            aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div>
-                        Кнопка
+        <Router>
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container-fluid">
+                        <a className="navbar-brand " href="#"><em>БыстрыйКонтракт</em></a>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                                aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"/>
+                        </button>
                     </div>
-                </div>
-            </nav>
-        </div>
+                </nav>
+            </div>
+            <Switch>
+                <Route
+                    path="/"
+                    render={({ match }) => <AuthPage />}
+                />
+            </Switch>
+        </Router>
     );
 };
 
